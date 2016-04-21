cb99114d8f0c6e8538e1d8e4a50d93cdef5eb3a0
ashraful88 <aaj.cse@gmail.com>
2016-04-21 07:54:05 -0400

    loader

--- app/etc/loader.php
+++ app/etc/loader.php
@@ -3,3 +3,13 @@
 * Phalcon loader
 */
 $loader = new Phalcon\Loader();
+
+/**
+ * We're a registering a set of directories
+ */
+$loader->registerNamespaces(array(
+    'Blue\Users\Controllers' => 'app/core/modules/users/controllers',
+    'Blue\Users\Models' => 'app/core/modules/users/models'
+));
+
+$loader->register();

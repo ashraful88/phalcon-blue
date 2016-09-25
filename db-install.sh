e41d331ca8dc85cd01fd3ad4b69ffca413eb67d6
Ashraful Abedein <ashraful@ekhanei.com>
2016-09-25 18:05:07 +0600

    user models

--- db-install.sh
+++ db-install.sh
@@ -5,7 +5,12 @@ sudo su postgres -c "psql -c 'CREATE DATABASE blue'"
 #sudo su postgres -c "psql blue -c 'DROP USER IF EXISTS pblue'"
 #sudo su postgres -c "psql blue -c 'CREATE USER pblue'"
 
-for file in $(find ./phalcon-blue/database -name "*.sql" | sort) ; do
-  echo "SQL file $file to database blue"
+for file in $(find ./phalcon-blue/database/schema -name "*.sql" | sort) ; do
+  echo "schema file $file to database blue"
+  sudo su postgres -c "psql blue < $file"
+done
+
+for file in $(find ./phalcon-blue/database/plsql -name "*.sql" | sort) ; do
+  echo "plsql file $file to database blue"
   sudo su postgres -c "psql blue < $file"
 done

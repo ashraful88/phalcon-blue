c32c7b35c2644e0260652e8d1192a570f028ca53
Ashraful Abedein <aaj.cse@gmail.com>
2018-01-28 21:05:59 +0800

    create account check

--- database/plsql/modules/00_Users/01__user_create.sql
+++ database/plsql/modules/00_Users/01__user_create.sql
@@ -12,6 +12,13 @@ BEGIN
     RAISE EXCEPTION '%', 'Blue:NULL_DATA:Blue';
   END IF;
 
+  IF EXISTS(SELECT 1
+            FROM user_accounts
+            WHERE phone = i_phone OR email = i_email)
+  THEN
+    RAISE EXCEPTION '%', 'Blue:USER_ACCOUNT_ALREADY_EXISTS:Blue';
+  END IF;
+
   INSERT INTO user_accounts (user_id, name, email, phone, password, status, params)
   VALUES (uuid_generate_v4(), i_name, i_email, i_phone, crypt(i_password, gen_salt('bf', 8)), i_status, i_params)
   RETURNING user_id
